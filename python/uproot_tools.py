f63981f2c441d336cb369c936cb360c4336d92d7
lfaldaul <lflaldaul@lxplus.cern.ch>
2023-06-07 13:16:05 +0200

    update uproot_tools.py

--- python/uproot_tools.py
+++ python/uproot_tools.py
@@ -22,8 +22,12 @@ def getTotalEventsInTree(filePathList, tree_name):
     Calculate total number of events in all files from a list with file paths 
     '''
     nevents = 0
-    for file_i, file_name in enumerate(filePathList):
-        file_uproot_i = uproot.open(filePathList[file_i])
+    if type(filePathList) == list:
+        for file_i, file_name in enumerate(filePathList):
+            file_uproot_i = uproot.open(filePathList[file_i])
+            nevents += len(file_uproot_i[tree_name].arrays("runNumber", library="pd"))
+    if type(filePathList) == str:
+        file_uproot_i = uproot.open(filePathList)
         nevents += len(file_uproot_i[tree_name].arrays("runNumber", library="pd"))
 
     return nevents
